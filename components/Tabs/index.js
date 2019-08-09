8560d12efb07d6ffcb814746708fa407e370b5b4
Raajn Patel <rndpatel@gmail.com>
2019-08-09 09:11:40 -0700

    Commit 3
    Tabs complete.
    Very similar setup to previous project
    forEach loop worked well to output the nav list.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        console.log(response.data);
+        let topics = response.data.topics;
+
+        tabCreate(topics);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+function tabCreate(topics) {
+    let topicDiv = document.querySelector(".topics");
+
+    topics.forEach(item => {
+        let tab = document.createElement("div");
+        tab.classList.add("tab");
+        tab.innerText = item;
+
+        topicDiv.appendChild(tab);
+    })
+}
